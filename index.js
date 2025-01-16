4d2aeba38f034cb56d10d4d7cb76cf46a6fbbe89
adnanahmed504423@gmail.comgit config --global user.email adnanahmed504423@gmail.com <adnanahmed504423@gmail.com>
2025-01-16 23:32:57 +0530

    commit

---
+++ index.js
@@ -0,0 +1,46 @@
+let express=require("express");
+let app=express();
+app.use(express.json());
+app.get("/",(req,res)=>{
+    res.send({
+        status:1,
+        msg:"home page api"
+    })
+})
+app.get("/news",(req,res)=>{
+    res.send({
+        status:2,
+        msg:"my second api"
+    })
+})
+app.get("/products",(req,res)=>{
+    res.send({
+        status:1,
+        msg:"products"
+    })
+})
+app.get("/news/:id",(req,res)=>{
+    let currentid=req.params.id
+    res.send("news detailsa api"+currentid)
+})
+
+app.post("/login",(req,res)=>{
+    console.log(req.body)
+    res.status(200).json(
+        {
+                    status:1,
+                    msg:"login successfully",
+                    bodydata:req.body,
+                    queryData:req.query
+                }
+    )
+//     res.send(
+//         {
+//         status:1,
+//         msg:"login successfully",
+//         bodydata:req.body,
+//         queryData:req.query
+//     }
+// )
+})
+app.listen("800")

2d40ffbddac8e86e4eaa0421d1d934c6b166a0ef
adnanahmed504423@gmail.comgit config --global user.email adnanahmed504423@gmail.com <adnanahmed504423@gmail.com>
2025-01-17 23:01:38 +0530

    commit

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.post("/login",(req,res)=>{
     res.status(200).json(
         {
                     status:1,
-                    msg:"login success",
+                    msg:"Login success",
                     bodydata:req.body,
                     queryData:req.query
                 }
